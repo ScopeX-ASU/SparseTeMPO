[20240502] Added Keep same for sparsity and fixed power calculation

core/models/dst2.py
@@ -246,6 +246,7 @@ class DSTScheduler2(nn.Module):
         skip_first_layer=True,
         skip_last_layer = True,
         update_frequency: int = 100,
+        keep_same: bool = False,
         T_max: int = 10000,
         group: str = "layer",  # layer, block wise magnitude sorting
         splitter_biases: float | List[int] = 90,
@@ -328,6 +329,7 @@ class DSTScheduler2(nn.Module):
         self.ADC_power = ADC_power
         self.TIA_power = TIA_power
         self.HDAC_power = HDAC_power
+        self.keep_same = keep_same
 
         # stats
         self.name2zeros = {}
@@ -416,6 +418,7 @@ class DSTScheduler2(nn.Module):
                     m.prune_mask = self.masks[
                         name_cur
                     ]  # the layer needs the mask to perform forward computation, pruning the weight is not enough.
+                    self.layers[name_cur] = m
                     m.register_buffer(f"row_prune_mask", m.prune_mask["row_mask"])
                     m.register_buffer(f"col_prune_mask", m.prune_mask["col_mask"])
             logger.info(f"created pruning mask.")
@@ -450,11 +453,11 @@ class DSTScheduler2(nn.Module):
     ) -> None:
         pruning_type = pruning_type or self.pruning_type
         if pruning_type == "unstructure":
-            self.update_and_apply_mask()
+            self.update_and_apply_mask(keep_same=self.keep_same)
             _, _ = self.update_fired_masks()
             self.print_nonzero_counts()
         elif pruning_type in {"structure_row", "structure_col", "structure_row_col"}:
-            self.update_and_apply_mask(pruning_type, indicator_list)
+            self.update_and_apply_mask(pruning_type, indicator_list, self.keep_same)
             _, _ = self.update_fired_masks(pruning_type="structure")
             self.print_nonzero_counts()
         else:
@@ -1008,7 +1011,9 @@ class DSTScheduler2(nn.Module):
     def _structure_init_random(self, density: float = 0.05) -> None:
         if self.pruning_type == "structure_row":
             for mask in self.masks.values():
-                mask["row_mask"].bernoulli_(p=density)
+                # mask["row_mask"].bernoulli_(p=density)
+                mask["row_mask"] = self.generate_interleave_mask(mask, density, False, self.device)
+                print(mask["row_mask"])
 
         elif self.pruning_type == "structure_col":
             for mask in self.masks.values():
@@ -1296,13 +1301,13 @@ class DSTScheduler2(nn.Module):
             self.masks[name] = new_mask
 
     def update_and_apply_mask(
-        self, pruning_type: str | None = None, indicator_list=None
+        self, pruning_type: str | None = None, indicator_list=None, keep_same: bool = False
     ) -> None:
         # update pruning and growth masks
         pruning_type = pruning_type or self.pruning_type
-
-        self.update_death_mask(pruning_type)
-        self.update_growth_mask(pruning_type)
+        if not keep_same:
+            self.update_death_mask(pruning_type)
+            self.update_growth_mask(pruning_type)
         self.apply_mask()
 
     # remove part mask
@@ -1660,6 +1665,10 @@ class DSTScheduler2(nn.Module):
                 return mask
 
             ## till this point, we know self.death_opts = ["crosstalk"]
+            best_gain = float("-inf")
+            search_range = list(
+                combinations(range(num_row_select_candidates), num_row_select)
+            )
 
             old_layer_mzi_power = self.calc_weight_MZI_power(name, mask)
             def obj_fn(
@@ -2632,6 +2641,27 @@ class DSTScheduler2(nn.Module):
     """
                 UTILITY
     """
+
+    def generate_interleave_mask(self, mask: MultiMask, sparsity: float = 0.5, is_col: bool=False, device="cuda:0"):
+        p, q, r, _, k1, _ = mask["row_mask"].shape
+        _, _, _, c, _, k2 = mask["col_mask"].shape
+        
+        total_element = mask.data.numel()
+        row_turn_off_number = int(round((total_element - total_element * sparsity) / (c * k2) / (p * q)))
+        row_interleave_index = torch.tensor(list(range(0, r*k1, 2)) + list(range(1, r*k1, 2)))[:row_turn_off_number]
+        col_turn_off_number = int(round((total_element - total_element * sparsity) / (r * k1) / (p * q)))
+        col_interleave_index = torch.tensor(list(range(0, c*k2, 2)) + list(range(1, c*k2, 2)))[:col_turn_off_number]
+        row_mask = torch.ones(r*k1, dtype=bool)
+        col_mask = torch.ones(c*k2, dtype=bool)
+        row_mask[row_interleave_index] = 0
+        col_mask[col_interleave_index] = 0
+        row_mask = row_mask.reshape(r, 1, k1, 1).expand(p, q, -1, -1, -1, -1)
+        col_mask = col_mask.reshape(1, c, 1, k2).expand(p, q, -1, -1, -1, -1)
+        # if is_col:
+        #     mask["col_mask"] = col_mask.to(device)
+        # else:
+        #     mask["row_mask"] = row_mask.to(device)
+        return col_mask.to(device) if is_col else row_mask.to(device)
 
     def get_gradient_for_weights(self, weight):
         grad = weight.grad

core/models/layers/base_layer.py
@@ -312,7 +312,7 @@ class ONNBaseLayer(nn.Module):
                 else:
                     factor = torch.ones([p,q,1,c], device=self.device) # [p,q,1,c]
 
-                print(factor.mean())
+                # print(factor.mean())
                 if self._enable_output_power_gating:
                     row_mask = self.prune_mask["row_mask"]  # [p,q,r,1,k1,1]
                     row_mask = row_mask[..., 0, :, :].flatten(2, 3)  # [p,q,r*k1, 1]
@@ -323,14 +323,14 @@ class ONNBaseLayer(nn.Module):
                 factor = factor.permute(0, 2, 1, 3).flatten(0, 1)[
                     : x.shape[1]
                 ]  # [p*r*k1, q, c] -> [out_c, q, c]
-                print(row_mask.sum()/row_mask.numel())
-                print(factor.sum()/factor.numel())
+                # print(row_mask.sum()/row_mask.numel())
+                # print(factor.sum()/factor.numel())
 
                 std = factor.mul(k2**0.5).square().sum([-2, -1]).sqrt()
 
                 std *= self.output_noise_std # [out_c]
-                print("no light dist no out gating: std: ", np.sqrt(np.prod(self.weight.shape[1::2])) * self.output_noise_std)
-                print("w/ light dist w/ out gating: std: ", std.mean().item(), std.min().item(), std.max().item())
+                # print("no light dist no out gating: std: ", np.sqrt(np.prod(self.weight.shape[1::2])) * self.output_noise_std)
+                # print("w/ light dist w/ out gating: std: ", std.mean().item(), std.min().item(), std.max().item())
 
                 noise = torch.randn_like(x)  # [bs, out_c, h, w] or [bs, out_c, q, c]
 

crosstalk_spacing.py
@@ -436,14 +436,26 @@ def main() -> None:
 
         mzi_total_energy, mzi_energy_dict, _, cycle_dict, _, _ = model.calc_weight_MZI_energy(next(iter(test_loader))[0].shape, R=R, C=C, freq=work_freq)
 
+        for name, m in model.named_modules():
+            if isinstance(m, model._conv):  # no last fc layer
+                if m.prune_mask is not None:
+                    print(m.prune_mask["row_mask"].cpu().numpy())
+
         total_cycles = 0
         for key, value in cycle_dict.items():
             total_cycles += value[0]  
 
-        layer_energy, layer_energy_breakdown, newtwork_energy_breakdown, total_energy = hw.calc_total_energy(cycle_dict, dst_scheduler, model)
+        # layer_energy, layer_energy_breakdown, newtwork_energy_breakdown, total_energy = hw.calc_total_energy(cycle_dict, dst_scheduler, model)
         
         acc_list = []
         avg_power_list = []
+        total_energy_list = []
+        layer_energy_list = []
+        layer_energy_breakdown_list = []
+        network_energy_breakdown_list = []
+        layer_power_list = []
+        layer_power_breakdown_list = []
+        network_power_breakdown_list = []
         for interv_s in interv_s_range:
             for interv_h_s in interv_h_s_range:
                 interv_h = interv_h_s + interv_s + model.crosstalk_scheduler.ps_width
@@ -472,24 +484,92 @@ def main() -> None:
                 acc_list.append((interv_s, interv_h_s, acc))
                 print(f"interv_s: {interv_s}, interv_h: {interv_h}, acc: {acc}")
             # next(iter(test_loader))[0].shape
+            layer_energy, layer_energy_breakdown, newtwork_energy_breakdown, total_energy = hw.calc_total_energy(cycle_dict, dst_scheduler, model)
             mzi_total_energy, mzi_energy_dict, _, _, _, _ = model.calc_weight_MZI_energy(next(iter(test_loader))[0].shape, R=R, C=C, freq=work_freq)
             for key in layer_energy:
                 layer_energy[key] += mzi_energy_dict[key]
                 layer_energy_breakdown[key]["MZI Power"] = mzi_energy_dict[key]
-                newtwork_energy_breakdown["MZI Power"] = mzi_total_energy
-                total_energy += mzi_total_energy
+             
+            newtwork_energy_breakdown["MZI Power"] = mzi_total_energy
+            total_energy += mzi_total_energy
 
+            layer_energy_np = np.array(list(layer_energy.items()), dtype='object')
+            layer_energy_breakdown_np = np.array(list(layer_energy_breakdown.items()), dtype='object')
+            newtwork_energy_breakdown_np = np.array(list(newtwork_energy_breakdown.items()), dtype='object')
+
+            layer_energy_list.append(layer_energy_np)
+            layer_energy_breakdown_list.append(layer_energy_breakdown_np)
+            network_energy_breakdown_list.append(newtwork_energy_breakdown_np)
+            total_energy_list.append(total_energy)
+
+            print(layer_energy)
+            print(layer_energy_breakdown)
+            print(newtwork_energy_breakdown)
+            print(total_energy)
+
+
+            for layer, components in layer_energy_breakdown.items():
+                    # print(layer_energy[layer])
+                    layer_energy[layer] = layer_energy[layer]/ (cycle_dict[layer][0] / work_freq / 1e9)
+                    for component, value in components.items():
+                            components[component] = value / (cycle_dict[layer][0] / work_freq / 1e9)
+
+            for key, value in newtwork_energy_breakdown.items():
+                newtwork_energy_breakdown[key] = value / (total_cycles / work_freq / 1e9)
+
+            total_energy = total_energy / (total_cycles / work_freq / 1e9)
             
+            print(layer_energy)
+            print(layer_energy_breakdown)
+            print(newtwork_energy_breakdown)
+            print(total_energy)
+
+            layer_power_np = np.array(list(layer_energy.items()), dtype='object')
+            layer_power_breakdown_np = np.array(list(layer_energy_breakdown.items()), dtype='object')
+            newtwork_power_breakdown_np = np.array(list(newtwork_energy_breakdown.items()), dtype='object')
+
+            layer_power_list.append(layer_power_np)
+            layer_power_breakdown_list.append(layer_power_breakdown_np)
+            network_power_breakdown_list.append(newtwork_power_breakdown_np)
+            avg_power_list.append(total_energy)
 
 
         acc_list = np.array(acc_list)
-        avg_power_list = np.array(avg_power_list)
-        print(acc_list.tolist())
-        print(avg_power_list.tolist())
 
-        np.savetxt(f"./log/fmnist/cnn/test_structural_pruning_without_optimization/{configs.loginfo}.csv",  acc_list, delimiter=",", fmt="%.2f")
-        np.savetxt(f"./log/fmnist/cnn/test_structural_pruning_without_optimization/{configs.loginfo}_acc_matrix.csv",  acc_list[:, -1].reshape([-1, interv_h_s_range.shape[0]]), delimiter=",", fmt="%.2f")
-        np.savetxt(f"./log/fmnist/cnn/test_structural_pruning_without_optimization/{configs.loginfo}_avgpower_list.csv",  avg_power_list, delimiter=",", fmt="%.2f")
+        avg_power_list = np.array(avg_power_list)
+        layer_power_list = np.array(layer_power_list)
+        layer_power_breakdown_list = np.array(layer_power_breakdown_list)
+        network_power_breakdown_list = np.array(network_power_breakdown_list)
+
+        layer_energy_list = np.array(layer_energy_list)
+        layer_energy_breakdown_list = np.array(layer_energy_breakdown_list)
+        network_energy_breakdown_list = np.array(network_energy_breakdown_list)
+        total_energy_list = np.array(total_energy_list)
+
+        # print(acc_list.tolist())
+        # print(avg_power_list.tolist())
+        # print(layer_power_list.tolist())
+        # print(layer_power_breakdown_list.tolist())
+        # print(network_power_breakdown_list.tolist())
+        # print(layer_energy_list.tolist())
+        # print(layer_energy_breakdown_list.tolist())
+        # print(network_energy_breakdown_list.tolist())
+        # print(total_energy_list.tolist())
+
+
+
+
+        np.savetxt(f"{configs.loginfo}.csv",  acc_list, delimiter=",", fmt="%.2f")
+        np.savetxt(f"{configs.loginfo}_acc_matrix.csv",  acc_list[:, -1].reshape([-1, interv_h_s_range.shape[0]]), delimiter=",", fmt="%.2f")
+        np.savetxt(f"{configs.loginfo}_avgpower_list.csv",  avg_power_list, delimiter=",", fmt="%.2f")
+        np.savetxt(f"{configs.loginfo}_network_total_energy_list.csv", total_energy_list, delimiter=",", fmt="%.2f")
+        np.savetxt(f"{configs.loginfo}_layer_power_list.csv", total_energy_list, delimiter=",", fmt="%.2f")
+        np.savetxt(f"{configs.loginfo}_layer_energy_list.csv", total_energy_list, delimiter=",", fmt="%.2f")
+        # np.savetxt(f"{configs.loginfo}_layer_energy_breakdown_list.txt", total_energy_list)
+        # np.savetxt(f"{configs.loginfo}_layer_power_breakdown_list.txt", total_energy_list)
+        # np.savetxt(f"{configs.loginfo}_network_energy_breakdown_list.txt", total_energy_list)
+        # np.savetxt(f"{configs.loginfo}_network_power_breakdown_list.txt", total_energy_list)
+
 
     except KeyboardInterrupt:
         lg.warning("Ctrl-C Stopped")